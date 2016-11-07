a5a65882399584498d5d8c2a294c7d510a4e4515
gaochundong <gaochundong@hotmail.com>
2016-11-07 22:18:00 +0800

    delete the code files

--- Happer/Happer.Rpc/IRpcServiceResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Happer.Rpc
-{
-    public interface IRpcServiceResolver
-    {
-        IRpcService<TRequest, TResponse> GetRpcService<TRequest, TResponse>()
-            where TRequest : class, new()
-            where TResponse : class, new();
-    }
-}
--- Happer/Happer.Rpc/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Happer.Rpc")]
-[assembly: Guid("e556f732-a488-4bf2-ab14-c0bfe4fd1db3")]
